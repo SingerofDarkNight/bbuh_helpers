ec8b39307359bd5a635e50944692d8781dbdebf9
luciusgone <luciusgone@gmail.com>
2018-12-24 20:37:12 +0800

    Hiding Specific content Feature(#3)
    1. Finished misc features
    * hiding medals;
    * hiding signatures;
    * hiding user replies by user level groups;
    
    2. Add a settings page;
    
    3. Bump version to 0.0.2

--- js/background.js
+++ js/background.js
@@ -12,3 +12,26 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 chrome.cookies.onChanged.addListener(function (changeInfo) {
     console.log("Cookie Changed " + JSON.stringify(changeInfo.cookie));
 });
+
+// setting up required info
+chrome.runtime.onInstalled.addListener(function (details) {
+    chrome.storage.local.get(['profiles', 'misc', 'settings'], function (items) {
+        if (Object.keys(items).length == 0) {
+            chrome.storage.local.set({
+                profiles: {},
+                misc: {
+                    hide_medals: false,
+                    hide_signs: false,
+                    hide_replies: false,
+                    min_level: 0
+                },
+                settings: {
+                    // TODO: prepopulating the rigid data might not be a good
+                    // idea
+                    user_levels: [9, 32, 10, 11, 12, 13, 14, 15, 28, 29, 30, 31],
+                    main_post_label: '基地'
+                }
+            });
+        }
+    });
+});
